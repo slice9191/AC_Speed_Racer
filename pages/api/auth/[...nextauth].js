bc4c0f4b16718a9219698239f1d0e3e7132caaa3
Thomas Lavoie <lavoiestudios919@gmail.com>
2025-04-27 15:19:59 -0700

    Really fix env variables this time

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,21 +1,19 @@
-import NextAuth from "next-auth"
-import DiscordProvider from "next-auth/providers/discord"
+import NextAuth from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
 
 export default NextAuth({
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET,      
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
       authorization: { params: { scope: 'identify guilds guilds.members.read' } }
-    })
+    }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async session({ session, token }) {
-      session.user.id = token.sub; // Attach Discord user ID to session
+    async session({ session, token, user }) {
+      session.user.id = token.sub;
       return session;
     }
   }
-  secret: process.env.NEXTAUTH_SECRET,
-
-  nextAuthUrl: process.env.https://ac-speed-racer.vercel.app/download,
-})
+});
