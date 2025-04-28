9f47e455846685a25d646c223401cb0ca531ca35
Thomas Lavoie <lavoiestudios919@gmail.com>
2025-04-28 14:56:03 -0700

    update download file version

--- pages/api/get-download-url.js
+++ pages/api/get-download-url.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     if (!file) return res.status(400).json({ error: "Missing file name" });
   
     // ⚠️ Make sure your file is public in R2
-    const publicUrl = `https://pub-868e0c5879a74d939d52af6ebd8a8dcc.r2.dev/StormLine%20Raceway.7z`;
+    const publicUrl = `https://pub-868e0c5879a74d939d52af6ebd8a8dcc.r2.dev/StormLine%20Raceway%200.8.7z`;
   
     return res.status(200).json({ url: publicUrl });
   }
